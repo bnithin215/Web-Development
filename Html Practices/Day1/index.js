3104808f8ae9960c416d61c405be96cf1a2221ff
bnithin215 <nithinbuggargani@gmail.com>
2025-01-15 23:46:41 +0530

    1st Week

---
+++ Html Practices/Day1/index.js
@@ -0,0 +1,17 @@
+// const arr=[2,3,4,5,6]
+// console.log(arr)
+// console.log(arr.slice(0,1));
+// console.log(!"Helllo");
+// console.log(+false);
+
+// console.log(x);
+// console.log(getName);
+// var x=7;
+// function getName(){
+//     console.log("Hello")
+// }
+
+
+function getElementByID(){
+    
+}
